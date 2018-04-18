b2059eb9196892c61df7d49143509a80f75ab66a
Tobias Wolter <towo@towo.eu>
2018-04-19 00:37:36 +0200

    Style adjustments in taskd spec

--- spec/classes/taskd_spec.rb
+++ spec/classes/taskd_spec.rb
@@ -4,14 +4,20 @@ describe 'taskd' do
   on_supported_os.each do |os, os_facts|
     context "on #{os}" do
       let(:facts) { os_facts }
-      let(:params) { {
-        'pki_vars' => {
-          'organization' => 'Testing Automation',
-          'country' => 'DE',
-          'state' => 'North Rhine-Westphalia',
-          'locality' => 'Cologne',
+      let(:params) do
+        {
+          'pki_vars' => {
+            'organization' => 'Testing Automation',
+            'country' => 'DE',
+            'state' => 'North Rhine-Westphalia',
+            'locality' => 'Cologne',
+            'cn' => 'somename.example.com',
+            'bits' => 4096,
+            'expiration_days' => 3650,
+          },
         }
-      } }
+      end
+
       it { is_expected.to compile }
     end
   end
