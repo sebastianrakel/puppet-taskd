e11b7c60daf704d867baf2f8ad862268f173f001
Tobias Wolter <towo@towo.eu>
2018-04-21 23:04:58 +0200

    Allow ensuring a user to be absent
    This adds the `ensure` parameter to `taskd::user`, with the states
    `present` or `absent`.
    
    `present` is the current behaviour, `absent` remove the user instead,
    deleting the org if it's empty then, and also removes the certificates
    of the user.
    
    Fixes #4.

--- manifests/user.pp
+++ manifests/user.pp
@@ -9,6 +9,7 @@
 define taskd::user(
   String $user = $name,
   String $org = $name,
+  Enum['present', 'absent'] $ensure = 'present',
 ) {
   # TODO is there a better way to do this without falling back to ::params?
   $config = lookup('taskd::config')
@@ -17,25 +18,47 @@ define taskd::user(
   $owner = lookup('taskd::owner')
   $group = lookup('taskd::group')
 
-  exec { "Create org ${org} if necessary":
-    command => "${taskd_executable} add --data ${config['root']} org ${org}",
-    onlyif  => "/usr/bin/test ! -d ${config['root']}/orgs/${org}",
-    user    => $owner,
-  }
+  if $ensure == present {
+    exec { "Create org ${org} if necessary":
+      command => "${taskd_executable} add --data ${config['root']} org ${org}",
+      onlyif  => "/usr/bin/test ! -d ${config['root']}/orgs/${org}",
+      user    => $owner,
+    }
 
-  exec { "Create user ${user} if necessary":
-    command => "${taskd_executable} add --data ${config['root']} user ${org} ${user}",
-    unless  => "/bin/grep '^user=${user}$' -r ${config['root']}/orgs/${org}",
-    user    => $owner,
-  }
+    exec { "Create user ${user} if necessary":
+      command => "${taskd_executable} add --data ${config['root']} user ${org} ${user}",
+      unless  => "/usr/bin/grep '^user=${user}$' -r ${config['root']}/orgs/${org}",
+      user    => $owner,
+    }
 
-  # Sadly, taskd does bad SSL where the user certificate is yet another
-  # certificate for the server, just a new key.  This would need a
-  # institutional fix, so we're running with it at the moment.
-  exec { "Create certificate for ${user}":
-    command => "${pki_base_dir}/generate.client ${org}_${user}",
-    cwd     => $config['root'],
-    creates => "${config['root']}/${org}_${user}.cert.pem",
-    user    => $owner,
+    # Sadly, taskd does bad SSL where the user certificate is yet another
+    # certificate for the server, just a new key.  This would need a
+    # institutional fix, so we're running with it at the moment.
+    exec { "Create certificate for ${user} if necessary":
+      command => "${pki_base_dir}/generate.client ${org}_${user}",
+      cwd     => $config['root'],
+      creates => "${config['root']}/${org}_${user}.cert.pem",
+      user    => $owner,
+    }
+  }
+  else {
+    exec { "Delete user ${user} if necessary":
+      command => "${taskd_executable} remove ${org} ${user}",
+      onlyif  => "/usr/bin/grep '^user=${user}$' -r ${config['root']}/orgs/${org}",
+      user    => $owner,
+    }
+
+    exec { "Delete org ${org} if empty":
+      # command => "${taskd_executable} remove ${org}",
+      command => '/bin/echo foo',
+      onlyif  => "/usr/bin/test -z \"\$(ls -A ${config['root']}/orgs/${org})\"",
+    }
+
+    file { [
+      "${config['root']}/${org}_${user}.cert.pem",
+      "${config['root']}/${org}_${user}.key.pem"
+    ]:
+      ensure => absent,
+    }
   }
 }
--- spec/defines/user_spec.rb
+++ spec/defines/user_spec.rb
@@ -9,9 +9,62 @@ describe 'taskd::user' do
   on_supported_os.each do |os, os_facts|
     context "on #{os}" do
       let(:facts) { os_facts }
+      let(:params) do
+        {
+          'user' => 'alice',
+          'org' => 'acme',
+        }
+      end
+
+      case os
+      when %r{^debian}i
+        taskd_data_root = '/var/lib/taskd'
+        taskd_user = 'Debian-taskd'
+
+      when %r{^ubuntu}i
+        taskd_data_root = '/var/lib/taskd'
+        taskd_user = 'Debian-taskd'
+
+      end
 
       it do
         is_expected.to compile
+      end
+
+      context 'with ensure => present' do
+        let(:params) do
+          super().merge('ensure' => 'present')
+        end
+
+        it { is_expected.to contain_exec("Create org #{params['org']} if necessary").with_user(taskd_user) }
+        it { is_expected.to contain_exec("Create user #{params['user']} if necessary").with_user(taskd_user) }
+        it do
+          is_expected.to contain_exec("Create certificate for #{params['user']} if necessary").with(
+            'cwd'     => taskd_data_root,
+            'user'    => taskd_user,
+            'creates' => "#{taskd_data_root}/#{params['org']}_#{params['user']}.cert.pem",
+          )
+        end
+
+        it { is_expected.not_to contain_exec("Delete user #{params['user']} if necessary") }
+        it { is_expected.not_to contain_exec("Delete certificate for #{params['user']}") }
+        it { is_expected.not_to contain_exec("Delete org #{params['org']} if empty") }
+      end
+
+      context 'with ensure => absent' do
+        let(:params) do
+          super().merge('ensure' => 'absent')
+        end
+
+        it { is_expected.not_to contain_exec("Create org #{params['org']} if necessary") }
+        it { is_expected.not_to contain_exec("Create user #{params['user']} if necessary") }
+        it { is_expected.not_to contain_exec("Create certificate for #{params['user']} if necessary") }
+
+        it { is_expected.to contain_exec("Delete user #{params['user']} if necessary") }
+        it { is_expected.to contain_exec("Delete org #{params['org']} if empty") }
+
+        it { is_expected.to contain_file("#{taskd_data_root}/#{params['org']}_#{params['user']}.cert.pem").with_ensure('absent') }
+        it { is_expected.to contain_file("#{taskd_data_root}/#{params['org']}_#{params['user']}.key.pem").with_ensure('absent') }
       end
     end
   end
