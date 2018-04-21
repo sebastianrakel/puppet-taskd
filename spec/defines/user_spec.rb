55fff33dabab050018d62c46c9c703d3b3919dd4
Tobias Wolter <towo@towo.eu>
2018-04-21 23:22:15 +0200

    Remove useless code
    Did somebody say something about code coverage?

--- spec/defines/user_spec.rb
+++ spec/defines/user_spec.rb
@@ -2,9 +2,6 @@ require 'spec_helper'
 
 describe 'taskd::user' do
   let(:title) { 'namevar' }
-  let(:params) do
-    {}
-  end
 
   on_supported_os.each do |os, os_facts|
     context "on #{os}" do
